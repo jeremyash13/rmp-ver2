9b258f8f9b59ed255449fe481bf93e93d4bdbae5
Jeremy <jeremyash13@gmail.com>
2020-04-30 16:57:31 -0600

    began working on admin dashboard

--- gatsby-site/src/components/modules/ArtManagement.js
+++ gatsby-site/src/components/modules/ArtManagement.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react"
-import CSS from "../../css/ArtManagement.css"
 import ArtContainer from "../state/ArtContainer"
+
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core"
 
 export const ArtManagement = () => {
   const GlobalState = ArtContainer.useContainer()
@@ -12,8 +14,8 @@ export const ArtManagement = () => {
 
     const fetchArt = new Promise(async (resolve, reject) => {
       try {
-        const url = "https://rmpdemo-backend.herokuapp.com/art"
-        // const url = "http://localhost:3000/art"
+        // const url = "https://rmpdemo-backend.herokuapp.com/art"
+        const url = "http://localhost:3000/art"
         const result = await fetch(url, {
           method: "POST",
           headers: {
@@ -32,29 +34,74 @@ export const ArtManagement = () => {
       })
   }, [])
 
-  return (
-    <div id="art-management">
-      <div className="item__wrapper">
-        <div className="item">{""}</div>
-        <div className="item">{"Title"}</div>
-        <div className="item">{"Artist"}</div>
-      </div>
-      {GlobalState.fetchedArt.map(item => {
-        return (
-          <div className="item__wrapper">
-            <div className="item">
-              <input type="checkbox"></input>
-            </div>
+  const style = css`
+    max-width: 1268px;
+    margin: 0 auto;
+    background-color: #eae9e9;
+    color: var(--text-black);
+    .entry-wrapper:hover {
+      cursor: pointer;
+      border: solid 1px var(--text-light-gray);
+      transform: scale(1.02);
+    }
+    ${'' /* .entry-wrapper:nth-child(2n) {
+      background-color: #dddddd;
+    } */}
+    thead tr {
+      height: 78px;
+    }
+    th {
+      background-color: #dddddd;
+      border: none;
+      font-weight: 400;
+      padding: 0;
+    }
+    td {
+      border: none;
+      font-weight: 300;
+      font-size: 0.9rem;
+      padding: 0;
+    }
+    .sku-item {
+      padding-right: 15px;
+    }
+    .title,
+    .artist,
+    .type {
+      text-transform: capitalize;
+    }
+  `
 
-            <div className="item">{item.title}</div>
-            <div className="item">{item.artist}</div>
-            <button className="item__button">Edit</button>
-          </div>
-        )
-      })}
-      <div className="item__wrapper">
-        <button className="item__button">Delete Selected</button>
-      </div>
-    </div>
+  return (
+    <table css={style} id="art-management">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Artist</th>
+          <th>SKU's</th>
+          <th>Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        {GlobalState.fetchedArt.map(item => {
+          return (
+            <tr className="entry-wrapper" data-item-id={item._id}>
+              <td className="title">{item.title}</td>
+              <td className="artist">{item.artist}</td>
+              <td className="sku-wrapper">
+                {item.options.map(i => (
+                  <td className="sku-item">
+                    <tr className="sku-code">{i.sku}</tr>
+                    <tr className="size">{i.size}</tr>
+                    <tr className="price">{i.price}</tr>
+                  </td>
+                ))}
+              </td>
+              <td className="type">{item.type}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   )
 }
--- gatsby-site/src/components/modules/Dashboard.js
+++ gatsby-site/src/components/modules/Dashboard.js
@@ -1,9 +1,12 @@
 import React from "react"
 import Layout from "../layout"
 import { ArtManagement } from "./ArtManagement"
+import SEO from "../../components/seo"
+
 const Dashboard = () => {
   return (
     <Layout>
+      <SEO title="Admin Dashboard" />
       <ArtManagement />
     </Layout>
   )
