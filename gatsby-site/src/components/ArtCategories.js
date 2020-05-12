af26c18ef9829678c082c12a99c946ac276796cb
Jeremy <jeremyash13@gmail.com>
2020-05-12 16:59:59 -0600

    minor style tweaks to ArtCategories

--- gatsby-site/src/components/ArtCategories.js
+++ gatsby-site/src/components/ArtCategories.js
@@ -1,18 +1,72 @@
-import React from 'react'
+import React, { useState } from "react"
 
 import ArtContainer from "./state/ArtContainer"
 
+/** @jsx jsx */
+import { css, jsx, Global } from "@emotion/core"
+
 export default function ArtCategories(props) {
   const GlobalState = ArtContainer.useContainer()
+  const [allActive, setAllActive] = useState(true)
+  const [landscapeActive, setLandscapeActive] = useState(false)
+  const [westernActive, setWesternActive] = useState(false)
+  const [wildlifeActive, setWildlifeActive] = useState(false)
+  const [patrioticActive, setPatrioticActive] = useState(false)
+  const style = css`
+    & .active {
+        color: red;
+    }
+  `
+
   return (
-        <div className={props.className}>
-            <ul>
-                <li onClick={() => {GlobalState.handleCategory('all')}}>ALL</li>
-                <li onClick={() => {GlobalState.handleCategory('landscape')}}>LANDSCAPE</li>
-                <li onClick={() => {GlobalState.handleCategory('western')}}>WESTERN</li>
-                <li onClick={() => {GlobalState.handleCategory('wildlife')}}>WILDLIFE</li>
-                <li onClick={() => {GlobalState.handleCategory('patriotic')}}>PATRIOTIC</li>
-            </ul>
-        </div>
-    )
+    <div css={style} className={props.className}>
+      <ul>
+        <li
+          className={allActive ? "active" : ""}
+          onClick={() => {
+            GlobalState.handleCategory("all")
+            setAllActive(true)
+          }}
+        >
+          ALL
+        </li>
+        <li
+          className={landscapeActive ? "active" : ""}
+          onClick={() => {
+            GlobalState.handleCategory("landscape")
+            setLandscapeActive(true)
+          }}
+        >
+          LANDSCAPE
+        </li>
+        <li
+          className={westernActive ? "active" : ""}
+          onClick={() => {
+            GlobalState.handleCategory("western")
+            setWesternActive(true)
+          }}
+        >
+          WESTERN
+        </li>
+        <li
+          className={wildlifeActive ? "active" : ""}
+          onClick={() => {
+            GlobalState.handleCategory("wildlife")
+            setWildlifeActive(true)
+          }}
+        >
+          WILDLIFE
+        </li>
+        <li
+          className={patrioticActive ? "active" : ""}
+          onClick={() => {
+            GlobalState.handleCategory("patriotic")
+            setPatrioticActive(true)
+          }}
+        >
+          PATRIOTIC
+        </li>
+      </ul>
+    </div>
+  )
 }
